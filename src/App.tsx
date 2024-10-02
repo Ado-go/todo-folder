9c475124632915adf649c77b074375cd5f65f7bd
Adogo <gonda.ado@gmail.com>
2024-10-02 16:22:34 +0200

    add: new components in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import Folder from "./components/Folder";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <Folder />
       <div>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
