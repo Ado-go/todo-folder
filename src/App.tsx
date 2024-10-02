57f8c6c7df5f48731c3b31330357ba00c3494011
Adogo <gonda.ado@gmail.com>
2024-10-02 18:52:44 +0200

    fix: remove unused state and counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import { useState } from "react";
 import Folder from "./components/Folder";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Folder />
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
     </>
   );
 }
