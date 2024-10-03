311bf37c30c6fde3e0ce45841f6876cb97dd70c6
Adogo <gonda.ado@gmail.com>
2024-10-03 19:45:28 +0200

    add: note inserter and note structure

--- src/components/Folder.tsx
+++ src/components/Folder.tsx
@@ -1,17 +1,42 @@
 import Note from "./Note";
+import NoteInserter from "./NoteInserter";
+
+import { useState } from "react";
+
 import "../css/Folder.css";
 
 function Folder() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      content: "hello from note",
+    },
+    {
+      id: 2,
+      content: "hello from another note",
+    },
+  ]);
+
+  const lastNoteId =
+    notes[notes.length - 1] === undefined ? 0 : notes[notes.length - 1].id;
+
+  const handleAddNewNote = () => {
+    setNotes((prev) => {
+      return [...prev, { id: lastNoteId + 1, content: "New note" }];
+    });
+  };
+
   return (
     <>
       <div className="tab"></div>
       <div className="folder">
         <div className="paper">
-          <Note />
-          <Note />
-          <Note />
-          <Note />
-          <Note />
+          {notes.map((note) => {
+            return <Note key={note.id} />;
+          })}
+          {notes.length < 9 && (
+            <NoteInserter onClickAddNote={handleAddNewNote} />
+          )}
         </div>
       </div>
     </>
